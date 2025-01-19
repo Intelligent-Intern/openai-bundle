70b3b3d00c0291391cab0359b99287a40175d132
Jochen Schultz <jschultz@php.net>
2025-01-19 15:30:42 +0100

    Update namespaces and docblocks for v1.0.0

--- src/OpenAIBundle.php
+++ src/OpenAIBundle.php
@@ -4,18 +4,13 @@ namespace IntelligentIntern\OpenAIBundle;
 
 use IntelligentIntern\OpenAIBundle\DependencyInjection\Compiler\AIServiceCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class OpenAIBundle extends AbstractBundle
 {
-    public function getPath(): string
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        return __DIR__;
-    }
-
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-        $container->addCompilerPass(new AIServiceCompilerPass());
+        $container->import(__DIR__ . '/../config/services.yaml');
     }
 }
